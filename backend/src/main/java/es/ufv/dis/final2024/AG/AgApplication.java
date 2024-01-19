17de02bc266fbb5aece6e6cc28824caff55b073c
AlvaroGarner <alvarogaertner10@gmail.com>
2024-01-19 19:16:26 +0100

    primer commit

---
+++ backend/src/main/java/es/ufv/dis/final2024/AG/AgApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.final2024.AG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgApplication.class, args);
+	}
+
+}
