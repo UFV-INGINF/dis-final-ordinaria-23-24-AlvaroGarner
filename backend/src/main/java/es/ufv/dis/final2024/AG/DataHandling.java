73fd32824a247885d0831276c938731bfd914c0d
AlvaroGarner <alvarogaertner10@gmail.com>
2024-01-19 19:48:43 +0100

    creacion del DataHandling

---
+++ backend/src/main/java/es/ufv/dis/final2024/AG/DataHandling.java
@@ -0,0 +1,19 @@
+package es.ufv.dis.final2024.AG;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+public class DataHandling {
+	// Función para escribir en json nueva información
+	boolean addData(NavesRaritas newData){
+		JsonReader reader = new JsonReader();
+		try{
+			ArrayList<NavesRaritas> dataList = reader.readJsonFile("backend/src/main/resources/data.json"); // Leemos fichero
+			dataList.add(newData); // Añadimos la información
+			reader.writeJsonFile("backend/src/main/resources/data.json", dataList);
+			return true;
+		}catch (Exception e){
+			return false;
+		}
+	}
+}
