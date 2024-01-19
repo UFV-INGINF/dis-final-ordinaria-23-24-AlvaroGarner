84d692d0d9555e3a5c762911e18c0c9d61a59402
AlvaroGarner <alvarogaertner10@gmail.com>
2024-01-19 19:31:58 +0100

    Cambio del puerto y creacion del controller

---
+++ backend/src/main/java/es/ufv/dis/final2024/AG/NavesController.java
@@ -0,0 +1,28 @@
+package es.ufv.dis.final2024.AG;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class NavesController {
+	// Get para lectura de fichero national, muestra los datos sin id
+	@GetMapping("/data")
+	public ArrayList<Naves> data(){
+		JsonReader reader = new JsonReader(); // Instancia de la clase JsonReader
+		return reader.readJsonFile("backend/src/main/resources/data.json"); // Leemos el fichero y lo devuelve
+	}
+
+	// Post para escritura de nuevos datos
+	@PostMapping(path="data",
+			consumes= MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Naves> createData(@RequestBody Naves newData){
+	// Pendiente de implementar
+}
