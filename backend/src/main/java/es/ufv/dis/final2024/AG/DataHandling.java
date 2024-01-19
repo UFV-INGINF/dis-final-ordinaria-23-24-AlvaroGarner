8a91982002afe251208a10f081d43d4f6a8c1499
AlvaroGarner <alvarogaertner10@gmail.com>
2024-01-19 20:49:43 +0100

    Creacion de funcion para buscar los datos por elname

--- backend/src/main/java/es/ufv/dis/final2024/AG/DataHandling.java
+++ backend/src/main/java/es/ufv/dis/final2024/AG/DataHandling.java
@@ -5,15 +5,18 @@ import java.util.List;
 import java.util.Map;
 public class DataHandling {
 	// Función para escribir en json nueva información
-	boolean addData(Naves newData){
+	// Función para encontrar la info por nombre
+	public Naves getDataInfo (String name){
+		Naves foundData = null;
 		JsonReader reader = new JsonReader();
-		try{
-			ArrayList<Naves> dataList = reader.readJsonFile("backend/src/main/resources/data.json"); // Leemos fichero
-			dataList.add(newData); // Añadimos la información
-			reader.writeJsonFile("backend/src/main/resources/data.json", dataList);
-			return true;
-		}catch (Exception e){
-			return false;
+
+		ArrayList<Naves> dataList = reader.readJsonFile("backend/src/main/resources/data.json"); // Lee el archivo JSON
+		for (Naves dataNat : dataList){
+			// Compara que los ID sean iguales
+			if (dataNat.getName().equalsIgnoreCase(name)){
+				foundData = dataNat;
+			}
 		}
+		return foundData;
 	}
 }
--- backend/src/main/java/es/ufv/dis/final2024/AG/NavesController.java
+++ backend/src/main/java/es/ufv/dis/final2024/AG/NavesController.java
@@ -18,7 +18,18 @@ public class NavesController {
 		JsonReader reader = new JsonReader(); // Instancia de la clase JsonReader
 		return reader.readJsonFile("backend/src/main/resources/data.json"); // Leemos el fichero y lo devuelve
 	}
-
+	
+	// Get para el name
+	@GetMapping("/data/name/{name}")
+	public ResponseEntity<Naves> getById(@PathVariable String name){
+		DataHandling dataHandling = new DataHandling(); // Instancia para utilizar métodos
+		Naves foundData = dataHandling.getDataInfo(name); // Encuentra la info por id
+		if (!(foundData == null)) {
+			return new ResponseEntity<>(foundData, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Devuelve 404 si no se encuentra el objeto
+		}
+	}
 	// Post para escritura de nuevos datos
 	@PostMapping(path="data",
 			consumes= MediaType.APPLICATION_JSON_VALUE,
